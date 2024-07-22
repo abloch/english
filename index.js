a900ae11e61ba02a621fffae3316ccd309d03f28
Akiva <bloch.akiva@gmail.com>
2024-07-22 21:32:55 +0300

    authentication with google

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
-<script type="module">
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-app.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-analytics.js";
-  import { getAuth } from "firebase/auth";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-app.js";
+import { getAuth, GoogleAuthProvider, signInWithPopup } from 'https://www.gstatic.com/firebasejs/10.12.4/firebase-auth.js'
 
-  const firebaseConfig = {
+const firebaseConfig = {
     apiKey: "AIzaSyB423RpTNa4DkBd3KMyU_doz3b43CmZtdw",
     authDomain: "english-quiz-fda3b.firebaseapp.com",
     databaseURL: "https://english-quiz-fda3b-default-rtdb.europe-west1.firebasedatabase.app",
@@ -13,10 +10,21 @@
     messagingSenderId: "363405286033",
     appId: "1:363405286033:web:03b7e8d33c9b9e36703d30",
     measurementId: "G-3GVTQRVYBT"
-  };
+};
 
-  // Initialize Firebase
-  const firebaseapp = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(firebaseapp);
-  const auth = getAuth(firebaseapp);
-</script>
+const firebaseapp = initializeApp(firebaseConfig);
+const auth = getAuth(firebaseapp);
+
+const provider = new GoogleAuthProvider();
+provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
+
+
+
+
+async function fblogin() {
+    const userCred = await signInWithPopup(auth, provider);
+    console.log(userCred?._tokenResponse);
+    return userCred?._tokenResponse;
+}
+
+window.fblogin = fblogin;
