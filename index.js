80875409d5d64d10fb979240a7876b01e0f542c5
Akiva <bloch.akiva@gmail.com>
2024-07-22 12:45:59 +0300

    add fake auth, still need to implement

---
+++ index.js
@@ -0,0 +1,22 @@
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-analytics.js";
+  import { getAuth } from "firebase/auth";
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyB423RpTNa4DkBd3KMyU_doz3b43CmZtdw",
+    authDomain: "english-quiz-fda3b.firebaseapp.com",
+    databaseURL: "https://english-quiz-fda3b-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "english-quiz-fda3b",
+    storageBucket: "english-quiz-fda3b.appspot.com",
+    messagingSenderId: "363405286033",
+    appId: "1:363405286033:web:03b7e8d33c9b9e36703d30",
+    measurementId: "G-3GVTQRVYBT"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+  const auth = getAuth(app);
+</script>
